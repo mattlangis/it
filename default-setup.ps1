5b9b9392bb5de93879bb6fc24c425e24f02ed04f
Mathieu Langis <mathieu.langis@gmail.com>
2023-10-11 23:07:12 -0400

    Update default-setup.ps1

--- default-setup.ps1
+++ default-setup.ps1
@@ -1,2 +1,3 @@
 # Setup Teamviewer
+echo 'Setup Teamviewer'
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mattlangis/main/teamviewer-setup.ps1'))
